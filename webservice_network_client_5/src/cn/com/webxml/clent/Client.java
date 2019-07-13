ebc74ddfab089ad714d68fc64623c7e55b535318
jsonWang <sowly_wanglian@126.com>
2019-07-14 00:50:02 +0800

    2019-07-14提交

--- webservice_network_client_5/src/cn/com/webxml/clent/Client.java
+++ webservice_network_client_5/src/cn/com/webxml/clent/Client.java
@@ -8,28 +8,27 @@ import cn.com.webxml.EnglishChineseSoap;
 
 public interface Client {
 	public static void main(String[] args) {
-		//1.生成
+		// 1.生成
 		// <wsdl:service name="EnglishChinese">
-		EnglishChinese service=new EnglishChinese();
-		//2.实例
-		//<wsdl:portType name="EnglishChineseSoap">
-		EnglishChineseSoap port=service.getPort(EnglishChineseSoap.class);
-		//3.调用
-		//<wsdl:operation name="translatorReferString">
-		ArrayOfString fyInfo=port.translatorReferString("天平");
-		List<String> list=fyInfo.getString();
-		for (String str:list) {
-		System.out.println(str);	
+		EnglishChinese service = new EnglishChinese();
+		// 2.实例
+		// <wsdl:portType name="EnglishChineseSoap">
+		EnglishChineseSoap port = service.getPort(EnglishChineseSoap.class);
+		// 3.调用
+		// <wsdl:operation name="translatorReferString">
+		ArrayOfString fyInfo = port.translatorReferString("天平");
+		List<String> list = fyInfo.getString();
+		for (String str : list) {
+			System.out.println(str);
 		}
-		//4.调用方法2
-		System.out.println("----------------------------");	
-		//<wsdl:operation name="TranslatorString">
-		ArrayOfString fyInfo2=port.translatorString("水杯");
-		List<String> list2=fyInfo2.getString();
-		for (String str2:list2) {
-		System.out.println(str2);	
+		// 4.调用方法2
+		System.out.println("----------------------------");
+		// <wsdl:operation name="TranslatorString">
+		ArrayOfString fyInfo2 = port.translatorString("水杯");
+		List<String> list2 = fyInfo2.getString();
+		for (String str2 : list2) {
+			System.out.println(str2);
 		}
 
-		
 	}
 }
